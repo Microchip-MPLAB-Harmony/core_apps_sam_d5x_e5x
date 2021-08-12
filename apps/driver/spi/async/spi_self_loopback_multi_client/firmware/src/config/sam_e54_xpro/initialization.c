ae02cf0a136034714c193aada7b85ff88935cc94
Amit Raddi <amit.raddi@microchip.com>
2021-08-12 17:41:33 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_self_loopback_multi_client_sam_e54_xpro
    MH3-56363
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_e54_xpro/initialization.c
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_e54_xpro/initialization.c
@@ -58,7 +58,7 @@
 #pragma config BOD33USERLEVEL = 0x1c
 #pragma config BOD33_ACTION = RESET
 #pragma config BOD33_HYST = 0x2
-#pragma config NVMCTRL_BOOTPROT = 0xF
+#pragma config NVMCTRL_BOOTPROT = 0
 #pragma config NVMCTRL_SEESBLK = 0x0
 #pragma config NVMCTRL_SEEPSZ = 0x0
 #pragma config RAMECC_ECCDIS = SET
@@ -96,7 +96,7 @@ const DRV_SPI_PLIB_INTERFACE drvSPI0PlibAPI = {
     .writeRead = (DRV_SPI_PLIB_WRITE_READ)SERCOM6_SPI_WriteRead,
 
     /* SPI PLIB Transfer Status function */
-    .isBusy = (DRV_SPI_PLIB_IS_BUSY)SERCOM6_SPI_IsBusy,
+    .isTransmitterBusy = (DRV_SPI_PLIB_TRANSMITTER_IS_BUSY)SERCOM6_SPI_IsTransmitterBusy,
 
     /* SPI PLIB Callback Register */
     .callbackRegister = (DRV_SPI_PLIB_CALLBACK_REGISTER)SERCOM6_SPI_CallbackRegister,
@@ -135,11 +135,6 @@ const DRV_SPI_INIT drvSPI0InitData =
     /* SPI Client Objects Pool */
     .clientObjPool = (uintptr_t)&drvSPI0ClientObjPool[0],
 
-    /* DMA Channel for Transmit */
-    .dmaChannelTransmit = SYS_DMA_CHANNEL_NONE,
-
-    /* DMA Channel for Receive */
-    .dmaChannelReceive  = SYS_DMA_CHANNEL_NONE,
 
     /* SPI Queue Size */
     .transferObjPoolSize = DRV_SPI_QUEUE_SIZE_IDX0,
