1487fb5a10154690d695ce32d9953af5a125a25d
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-16 20:26:17 +0530

    [SYS_FS] Regenerated demo for fs_sdmmc_fat_sam_e54_xpro_freertos_keil
    MH3-65300
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/fs/sdmmc_fat/firmware/src/config/sam_e54_xpro_freertos_keil/interrupts.c
+++ apps/fs/sdmmc_fat/firmware/src/config/sam_e54_xpro_freertos_keil/interrupts.c
@@ -72,12 +72,11 @@ void __attribute__((section(".text.Dummy_Handler")))Dummy_Handler(void)
     {
     }
 }
+
+/* MISRAC 2012 deviation block start */
+/* MISRA C-2012 Rule 8.6 deviated 134 times.  Deviation record ID -  H3_MISRAC_2012_R_8_6_DR_1 */
 /* Device vectors list dummy definition*/
-extern void MemoryManagement_Handler   ( void ) __attribute__((weak, alias("Dummy_Handler")));
-extern void BusFault_Handler           ( void ) __attribute__((weak, alias("Dummy_Handler")));
-extern void UsageFault_Handler         ( void ) __attribute__((weak, alias("Dummy_Handler")));
 extern void vPortSVCHandler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
-extern void DebugMonitor_Handler       ( void ) __attribute__((weak, alias("Dummy_Handler")));
 extern void xPortPendSVHandler         ( void ) __attribute__((weak, alias("Dummy_Handler")));
 extern void PM_Handler                 ( void ) __attribute__((weak, alias("Dummy_Handler")));
 extern void MCLK_Handler               ( void ) __attribute__((weak, alias("Dummy_Handler")));
@@ -213,6 +212,7 @@ extern void QSPI_Handler               ( void ) __attribute__((weak, alias("Dumm
 extern void SDHC0_Handler              ( void ) __attribute__((weak, alias("Dummy_Handler")));
 
 
+/* MISRAC 2012 deviation block end */
 
 /* Multiple handlers for vector */
 
