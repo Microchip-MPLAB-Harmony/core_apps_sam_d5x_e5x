d5f12abef485f6e8031c4b9c49142f1818a78441
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-16 19:45:12 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_async_i2c_eeprom_sam_e54_xpro
    MH3-65270
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_e54_xpro/definitions.h
@@ -56,11 +56,11 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "driver/i2c/drv_i2c.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "driver/i2c/drv_i2c.h"
 #include "app.h"
 
 
