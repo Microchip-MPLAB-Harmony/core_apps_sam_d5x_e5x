5bb2aede164546ad05ca37ee78f02d71c2dfb25f
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-11 13:05:29 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_sam_e54_xpro_freertos
    MH3-76706

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_e54_xpro_freertos/peripheral/port/plib_port.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_e54_xpro_freertos/peripheral/port/plib_port.c
@@ -74,20 +74,20 @@ void PORT_Initialize(void)
 
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_OUT = 0x80000000;
-   PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x1;
-   PORT_REGS->GROUP[1].PORT_PINCFG[25] = 0x1;
-   PORT_REGS->GROUP[1].PORT_PINCFG[31] = 0x6;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x80000000U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x1U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[25] = 0x1U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[31] = 0x6U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x33;
-   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x33U;
+   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0U;
 
    /************************** GROUP 2 Initialization *************************/
-   PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
-   PORT_REGS->GROUP[2].PORT_OUT = 0x40001;
-   PORT_REGS->GROUP[2].PORT_PINCFG[18] = 0x0;
+   PORT_REGS->GROUP[2].PORT_DIR = 0x40000U;
+   PORT_REGS->GROUP[2].PORT_OUT = 0x40001U;
+   PORT_REGS->GROUP[2].PORT_PINCFG[18] = 0x0U;
 
-   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0U;
 
    /************************** GROUP 3 Initialization *************************/
 
--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_e54_xpro_freertos/sys_tasks.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_e54_xpro_freertos/sys_tasks.h
@@ -1,26 +1,23 @@
 /*******************************************************************************
-  SERCOM Universal Synchronous/Asynchrnous Receiver/Transmitter PLIB
+ System Tasks Header File
 
-  Company
-    Microchip Technology Inc.
+  File Name:
+    sys_tasks.h
 
-  File Name
-    plib_sercom2_usart.h
+  Summary:
+    This file contains declarations for task handles.
 
-  Summary
-    USART peripheral library interface.
-
-  Description
-    This file defines the interface to the USART peripheral library. This
-    library provides access to and control of the associated peripheral
-    instance.
+  Description:
+    Task handles declared in this header file can be used by the application
+    to control the behavior of the tasks.
 
   Remarks:
-    None.
-*******************************************************************************/
+    None
+ *******************************************************************************/
 
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2023 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -40,10 +37,11 @@
 * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*******************************************************************************/
+ *******************************************************************************/
+// DOM-IGNORE-END
 
-#ifndef PLIB_SERCOM2_USART_H // Guards against multiple inclusion
-#define PLIB_SERCOM2_USART_H
+#ifndef SYS_TASKS_H
+#define SYS_TASKS_H
 
 // *****************************************************************************
 // *****************************************************************************
@@ -51,64 +49,18 @@
 // *****************************************************************************
 // *****************************************************************************
 
-#include "plib_sercom_usart_common.h"
-
-// DOM-IGNORE-BEGIN
-#ifdef __cplusplus // Provide C++ Compatibility
-
-    extern "C" {
-
-#endif
-// DOM-IGNORE-END
+#include "configuration.h"
+#include "definitions.h"
 
 // *****************************************************************************
 // *****************************************************************************
-// Section: Interface Routines
+// Section: RTOS "Tasks" Handles
 // *****************************************************************************
 // *****************************************************************************
-
-void SERCOM2_USART_Initialize( void );
-
-bool SERCOM2_USART_SerialSetup( USART_SERIAL_SETUP * serialSetup, uint32_t clkFrequency );
-
-void SERCOM2_USART_Enable( void );
-
-void SERCOM2_USART_Disable( void );
-
-void SERCOM2_USART_TransmitterEnable( void );
-
-void SERCOM2_USART_TransmitterDisable( void );
-
-bool SERCOM2_USART_Write( void *buffer, const size_t size );
-
-bool SERCOM2_USART_TransmitComplete( void );
+/* Declaration of  APP_Tasks task handle */
+extern TaskHandle_t xAPP_Tasks;
 
 
-bool SERCOM2_USART_TransmitterIsReady( void );
-
-void SERCOM2_USART_WriteByte( int data );
 
 
-void SERCOM2_USART_ReceiverEnable( void );
-
-void SERCOM2_USART_ReceiverDisable( void );
-
-bool SERCOM2_USART_Read( void *buffer, const size_t size );
-
-bool SERCOM2_USART_ReceiverIsReady( void );
-
-int SERCOM2_USART_ReadByte( void );
-
-USART_ERROR SERCOM2_USART_ErrorGet( void );
-
-uint32_t SERCOM2_USART_FrequencyGet( void );
-
-// DOM-IGNORE-BEGIN
-#ifdef __cplusplus  // Provide C++ Compatibility
-
-    }
-
-#endif
-// DOM-IGNORE-END
-
-#endif //PLIB_SERCOM2_USART_H
+#endif //SYS_TASKS_H
