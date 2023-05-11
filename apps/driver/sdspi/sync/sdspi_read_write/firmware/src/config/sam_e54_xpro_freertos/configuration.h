d169a56a9c3148d92329180a4d453fbe41cc330c
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-11 13:05:09 +0530

    [DRV_SDSPI] Regenerated demo for driver_sdspi_sync_sdspi_read_write_sam_e54_xpro_freertos
    MH3-76671

--- apps/driver/sdspi/sync/sdspi_read_write/firmware/src/config/sam_e54_xpro_freertos/configuration.h
+++ apps/driver/sdspi/sync/sdspi_read_write/firmware/src/config/sam_e54_xpro_freertos/configuration.h
@@ -86,7 +86,7 @@ extern "C" {
 #define SYS_TIME_HW_COUNTER_PERIOD                  (0xFFFFU)
 #define SYS_TIME_HW_COUNTER_HALF_PERIOD             (SYS_TIME_HW_COUNTER_PERIOD>>1)
 #define SYS_TIME_CPU_CLOCK_FREQUENCY                (120000000)
-#define SYS_TIME_COMPARE_UPDATE_EXECUTION_CYCLES    (188)
+#define SYS_TIME_COMPARE_UPDATE_EXECUTION_CYCLES    (232)
 
 
 
@@ -112,7 +112,7 @@ extern "C" {
 #define DRV_SDSPI_PRIORITY_IDX0                     1
 
 /* SDSPI Driver Common Configuration Options */
-#define DRV_SDSPI_INSTANCES_NUMBER              1
+#define DRV_SDSPI_INSTANCES_NUMBER              (1U)
 
 
 
