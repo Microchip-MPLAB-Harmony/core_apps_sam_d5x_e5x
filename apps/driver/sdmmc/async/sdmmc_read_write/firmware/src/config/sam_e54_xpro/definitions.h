a5d06967e0f2f9bf2df44f741ccc3adde74b5a72
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-11 12:59:33 +0530

    [DRV_SDMMC] Regenerated demo for driver_sdmmc_async_sdmmc_read_write_sam_e54_xpro
    MH3-76669

--- apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_e54_xpro/definitions.h
@@ -58,11 +58,11 @@
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "peripheral/sdhc/plib_sdhc1.h"
-#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -74,6 +74,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAME54P20A"
+#define DEVICE_ARCH			 "CORTEX-M4"
+#define DEVICE_FAMILY		 "SAME"
+#define DEVICE_SERIES		 "SAME54"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 120000000
