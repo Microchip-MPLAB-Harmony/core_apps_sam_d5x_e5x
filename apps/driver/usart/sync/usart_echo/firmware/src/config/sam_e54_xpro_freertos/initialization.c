64ef2705d2ae164333bab92ea94c16d11d00e3fe
Vishal Gandhi <vishal.gandhi@microchip.com>
2024-11-07 17:52:27 +0530

    [DRV_] Regenerated demo for driver__usart__sync__usart_echo__firmware__sam_e54_xpro_freertos.X
    MH3-103790

--- apps/driver/usart/sync/usart_echo/firmware/src/config/sam_e54_xpro_freertos/initialization.c
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/sam_e54_xpro_freertos/initialization.c
@@ -48,7 +48,6 @@
 #include "device.h"
 
 
-
 // ****************************************************************************
 // ****************************************************************************
 // Section: Configuration Bits
@@ -79,9 +78,10 @@
 // *****************************************************************************
 // *****************************************************************************
 /* Following MISRA-C rules are deviated in the below code block */
-/* MISRA C-2012 Rule 11.1 */
-/* MISRA C-2012 Rule 11.3 */
-/* MISRA C-2012 Rule 11.8 */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 // <editor-fold defaultstate="collapsed" desc="DRV_USART Instance 0 Initialization Data">
 
 static DRV_USART_CLIENT_OBJ drvUSART0ClientObjPool[DRV_USART_CLIENTS_NUMBER_IDX0];
@@ -175,6 +175,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
     /* MISRAC 2012 deviation block start */
     /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
@@ -192,7 +193,6 @@ void SYS_Initialize ( void* data )
 
 	BSP_Initialize();
     EVSYS_Initialize();
-
 
 
     /* MISRAC 2012 deviation block start */
@@ -213,9 +213,7 @@ void SYS_Initialize ( void* data )
 
 
     /* MISRAC 2012 deviation block end */
-
 }
-
 
 /*******************************************************************************
  End of File
