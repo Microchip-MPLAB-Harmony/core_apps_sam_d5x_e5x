9f8408cac7638d53142629fb72ac24006f30e5c5
Vishal Gandhi <vishal.gandhi@microchip.com>
2024-11-07 17:48:19 +0530

    [DRV_SPI] Regenerated demo for driver__spi__async__spi_multi_instance__firmware__sam_e54_xpro.X
    MH3-103768

--- apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_e54_xpro/initialization.c
+++ apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_e54_xpro/initialization.c
@@ -48,7 +48,6 @@
 #include "device.h"
 
 
-
 // ****************************************************************************
 // ****************************************************************************
 // Section: Configuration Bits
@@ -79,9 +78,10 @@
 // *****************************************************************************
 // *****************************************************************************
 /* Following MISRA-C rules are deviated in the below code block */
-/* MISRA C-2012 Rule 11.1 */
-/* MISRA C-2012 Rule 11.3 */
-/* MISRA C-2012 Rule 11.8 */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 // <editor-fold defaultstate="collapsed" desc="DRV_SPI Instance 1 Initialization Data">
 
 /* SPI Client Objects Pool */
@@ -112,13 +112,13 @@ static const uint32_t drvSPI1remapClockPhase[] = { 0x10000000, 0x0 };
 
 static const DRV_SPI_INTERRUPT_SOURCES drvSPI1InterruptSources =
 {
-    /* Peripheral has more than one interrupt vectors */
+    /* Peripheral has more than one interrupt vector */
     .isSingleIntSrc                        = false,
 
     /* Peripheral interrupt lines */
-    .intSources.multi.spiTxReadyInt      = (int32_t)SERCOM6_0_IRQn,
-    .intSources.multi.spiTxCompleteInt   = (int32_t)SERCOM6_1_IRQn,
-    .intSources.multi.spiRxInt           = (int32_t)SERCOM6_2_IRQn,
+   .intSources.multi.spiTxReadyInt     = (int32_t)SERCOM6_0_IRQn,
+   .intSources.multi.spiTxCompleteInt  = (int32_t)SERCOM6_1_IRQn,
+   .intSources.multi.spiRxInt          = (int32_t)SERCOM6_2_IRQn,
     /* DMA Tx interrupt line */
     .intSources.multi.dmaTxChannelInt      = (int32_t)DMAC_2_IRQn,
     /* DMA Rx interrupt line */
@@ -195,13 +195,13 @@ static const uint32_t drvSPI0remapClockPhase[] = { 0x10000000, 0x0 };
 
 static const DRV_SPI_INTERRUPT_SOURCES drvSPI0InterruptSources =
 {
-    /* Peripheral has more than one interrupt vectors */
+    /* Peripheral has more than one interrupt vector */
     .isSingleIntSrc                        = false,
 
     /* Peripheral interrupt lines */
-    .intSources.multi.spiTxReadyInt      = (int32_t)SERCOM4_0_IRQn,
-    .intSources.multi.spiTxCompleteInt   = (int32_t)SERCOM4_1_IRQn,
-    .intSources.multi.spiRxInt           = (int32_t)SERCOM4_2_IRQn,
+   .intSources.multi.spiTxReadyInt     = (int32_t)SERCOM4_0_IRQn,
+   .intSources.multi.spiTxCompleteInt  = (int32_t)SERCOM4_1_IRQn,
+   .intSources.multi.spiRxInt          = (int32_t)SERCOM4_2_IRQn,
     /* DMA Tx interrupt line */
     .intSources.multi.dmaTxChannelInt      = (int32_t)DMAC_0_IRQn,
     /* DMA Rx interrupt line */
@@ -294,6 +294,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
     /* MISRAC 2012 deviation block start */
     /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
@@ -315,7 +316,6 @@ void SYS_Initialize ( void* data )
     DMAC_Initialize();
 
     SERCOM4_SPI_Initialize();
-
 
 
     /* MISRAC 2012 deviation block start */
@@ -342,9 +342,7 @@ void SYS_Initialize ( void* data )
 
 
     /* MISRAC 2012 deviation block end */
-
 }
-
 
 /*******************************************************************************
  End of File
