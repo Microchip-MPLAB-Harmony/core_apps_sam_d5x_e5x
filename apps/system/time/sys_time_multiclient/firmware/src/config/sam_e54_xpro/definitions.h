972a7fb0fdb11a88906ca49cd522e523e992bce4
Vishal Gandhi <vishal.gandhi@microchip.com>
2024-11-07 17:43:23 +0530

    [SYS_] Regenerated demo for system__time__sys_time_multiclient__firmware__sam_e54_xpro.X
    MH3-103836

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_e54_xpro/definitions.h
@@ -58,11 +58,11 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "peripheral/tc/plib_tc0.h"
-#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -76,13 +76,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAME54P20A"
-#define DEVICE_ARCH			 "CORTEX-M4"
-#define DEVICE_FAMILY		 "SAME"
-#define DEVICE_SERIES		 "SAME54"
+#define DEVICE_NAME          "ATSAME54P20A"
+#define DEVICE_ARCH          "CORTEX-M4"
+#define DEVICE_FAMILY        "SAME"
+#define DEVICE_SERIES        "SAME54"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 120000000
+#define CPU_CLOCK_FREQUENCY 120000000U
 
 // *****************************************************************************
 // *****************************************************************************
