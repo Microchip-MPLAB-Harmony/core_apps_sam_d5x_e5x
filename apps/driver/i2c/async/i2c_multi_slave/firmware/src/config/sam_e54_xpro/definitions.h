7c4328d2a691ff7b332dc2326da0d95809aa1441
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 09:33:15 +0000

    [APP] Regenerated demo for driver__i2c__async__i2c_multi_slave__firmware
    MH3C-344

--- apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_e54_xpro/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
+#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/sercom/i2c_master/plib_sercom7_i2c_master.h"
 #include "peripheral/port/plib_port.h"
@@ -57,8 +57,8 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "peripheral/tc/plib_tc0.h"
-#include "system/time/sys_time.h"
 #include "driver/i2c/drv_i2c.h"
+#include "system/time/sys_time.h"
 #include "bsp/bsp.h"
 #include "system/console/sys_console.h"
 #include "system/console/src/sys_console_uart_definitions.h"
